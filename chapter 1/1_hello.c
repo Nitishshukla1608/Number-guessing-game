52c7a738b1224fc6b2467afd970583690b58e6de
Nitishshukla1608 <shuklanitish057@gmail.com>
2024-11-27 18:56:06 +0530

    Delete chapter 1 directory
    this is unwanted part of this repository

--- chapter 1/1_hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int  nam = 8;
-    int NAM=1;
-   printf("hello world i am learning c with code with harry");
-  return 0;
-}
--- chapter 1/2_variables.c
+++
@@ -1,8 +0,0 @@
-# include <stdio.h>
-int main () {
-int a = 4;
-int b = 3.5;// not recommended bcoz it is a real variable
-char c = 'u';
-printf("hello %c",c);
-return 0;
-}
