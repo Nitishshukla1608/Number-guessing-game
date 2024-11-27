21a9e97eec2485408c76afe25295499ee0325ba6
Nitishshukla1608 <shuklanitish057@gmail.com>
2024-11-27 18:56:16 +0530

    Delete chapter 2 directory

--- chapter 2/04_quick_quiz.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-int main (){
-//int k = 3.0/9;
-//printf("%f",k); //float anf int give an float value so 0.33 will be the value of k but demoted to 0 bcoz int is used
-
-//float k = 3.0/9;
-//printf("%f",k); // it should return 0.333333 bcoz %f is used for floating output number which is true
-return 0;
-}
